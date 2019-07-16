cdc6ca1cc491faadb4ba4c58fd5b1de5c958eefb
greendb16 <greendb16@gmail.com>
2019-07-15 21:21:24 -0400

    exercise 304 not complete

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 4)
+    private String model;
+
+    @NotNull
+    @Size(min=4)
+    private String make;
+
+    @NotNull
+    @Min(1700)
+    private int year;
+
+    public Car() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
